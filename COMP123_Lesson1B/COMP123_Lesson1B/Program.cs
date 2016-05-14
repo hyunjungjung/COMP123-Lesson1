6d2be530edde63f2e10864c4835126c5f7038073
Joanne Jung <hjung3@my.centennialcollege.ca>
2016-05-13 20:59:17 -0400

    Initial Version

---
+++ COMP123_Lesson1B/COMP123_Lesson1B/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Author : Joanne Jung
+ * Date Created: May 13th, 2016
+ * Date Modified : May 13th, 2016
+ * Student ID : 300432364
+ * 
+ * Description : This program demonstrates GitHub and version control
+ * best practices. We also explore Unit Testing.
+ * 
+ * Version : 0.1 - Initial Version
+ */
+
+
+namespace COMP123_Lesson1B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
